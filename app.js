dd703a7b2484cd7e97d862b7e437bc529d009911
Leonid <dolgodvorov.l@yandex.ru>
2022-08-27 02:06:19 +0500

    очередной прогон после eslint

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
-const { NOT_FOUND_ERROR } = require('./utils/constants')
+const { NOT_FOUND_ERROR } = require('./utils/constants');
 
 const app = express();
 
